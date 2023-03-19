bd8b8e50e083805610ff55cbf2ce512a691657b9
Cristian Gustavo Vargas Rondal <cristianvargasplus@gmail.com>
2023-03-19 20:13:23 -0300

    agrego funciones aPesos() y aDolares()

--- ConvertidorPesosDolares/mainPesosDolares.js
+++ ConvertidorPesosDolares/mainPesosDolares.js
@@ -6,14 +6,29 @@ var id = (id) => document.getElementById(id);
 var classes = (classes) => document.getElementsByClassName(classes);
 
 
+var dolaresConvertidos = id("dolaresInput");
+var pesosConvertidos = id("pesosInput");
+var pesos = id("pesos");
+var btn_aPesos = id("btn_aPesos");
+var btn_aDolares = id("btn_aDolares");
+
+
 function convertirPesosADolares(pesos) {
     return  pesos / 300;
 }
 
 function convertirDolaresAPesos(dolares) {
     return  dolares * 300;
-
 }
+
+btn_aDolares.onclick= () => {
+    dolaresConvertidos.value = `$${convertirPesosADolares(pesos.value)}`;
+}
+function aPesos(dolares){
+    pesosConvertidos.value =`$${convertirDolaresAPesos(dolares)}`;
+}
+
+
 
 
 
