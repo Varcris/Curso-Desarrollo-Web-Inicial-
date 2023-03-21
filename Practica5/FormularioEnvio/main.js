20654720221eecc568d4647ccf7132e5c307541b
Cristian Gustavo Vargas Rondal <cristianvargasplus@gmail.com>
2023-03-21 11:38:11 -0300

    agrego mensaje de exito y el titulo

--- Practica5/FormularioEnvio/main.js
+++ Practica5/FormularioEnvio/main.js
@@ -1,18 +1,25 @@
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
+
 const form = id("form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let nombreCliente = id("nombreCliente").value;
+    let error = classes("error");
+    let mensaje = classes("mensaje");
     if (nombreCliente.trim() === "") {
-        classes("error")[0].innerHTML = "El nombre del cliente es obligatorio";
+        error[0].innerHTML = "El nombre del cliente es obligatorio";
+        mensaje[0].innerHTML = "";
         return;
-    }else
-        classes("error")[0].innerHTML = "";
+    }else{
+        error[0].innerHTML = "";
+        mensaje[0].innerHTML = "La solicitud se ha enviado correctamente";
+    }
     let materialCaja = id("materialCaja").value;
     let dimension = document.querySelector('input[name="dimensiones"]:checked').value;
     let comentario = id("comentario").value;
     let respuesta = id("respuesta");
     let data = `${nombreCliente} ha pedido una caja de ${materialCaja} con dimensiones ${dimension}. ${comentario}`;
     respuesta.value = data;
+   
 });
