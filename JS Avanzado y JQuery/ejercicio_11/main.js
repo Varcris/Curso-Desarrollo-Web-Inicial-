5c286dcaf27ce10c9a56ea5751df5ba4fd425c3c
Cristian Gustavo Vargas Rondal <cristianvargasplus@gmail.com>
2023-04-17 15:30:55 -0300

    agrego evento click y dbclick a filas de una tabla con Jquery

--- JS Avanzado y JQuery/ejercicio_11/main.js
+++ JS Avanzado y JQuery/ejercicio_11/main.js
@@ -1,3 +1,14 @@
 //11. Confeccionar una página que muestre una tabla con dos filas y cambiar el color de fondo
 //cuando sea presionada con el mouse.
 
+$(document).ready(function(){
+    
+    $("tbody > tr").click(function(){
+        $(this).css("background-color", "red");
+    });
+
+    $("tbody > tr").dblclick(function(){
+        $(this).css("background-color", "white");
+    });
+});
+
