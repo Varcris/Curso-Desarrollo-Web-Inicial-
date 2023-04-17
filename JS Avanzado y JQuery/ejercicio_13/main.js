7ca6fe74b42284d1d2e6bbe52cfa5967bf3af380
Cristian Gustavo Vargas Rondal <cristianvargasplus@gmail.com>
2023-04-17 17:48:02 -0300

    agrego event mousedown y mouseup a las filas de la primera tabla con Jquery

--- JS Avanzado y JQuery/ejercicio_13/main.js
+++ JS Avanzado y JQuery/ejercicio_13/main.js
@@ -1,3 +1,13 @@
 //13. Confeccionar una página que muestre dos tablas con tres filas cada una. Cambiar el color
 //de fondo de cada fila de la primera tabla cuando sea presionada con el mouse.
 
+$(document).ready(function(){
+
+    $("table:first > tbody > tr").mousedown(function(){
+        $(this).css("background-color", "red");
+    });
+    $("table:first > tbody > tr").mouseup(function(){
+        $(this).css("background-color", "white");
+    });
+});
+
